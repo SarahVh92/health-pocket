99f0ad2ae07ef02a941160ccf082c2170e4a50ce
Sarah Vanholsbeeck <vanholsbeeck.sn@gmail.com>
2023-02-07 22:56:57 +0900

    Added validations to User and Document

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   has_many :documents
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :phone_number, presence: true
+  validates :address, presence: true
 end
