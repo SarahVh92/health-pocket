c4ff2573425a57642c2c203e96dbc13b8471d6b3
Soares Kim <kimuedasoares@gmail.com>
2023-02-09 21:13:17 +0900

    seedsfile2

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :documents
+  has_many :documents, dependent: :destroy
 
   validates :first_name, presence: true
   validates :last_name, presence: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,14 @@
 #   Character.create(name: "Luke", movie: movies.first)
 require 'open-uri'
 
-def grab_image(url, offer)
+def grab_image(url, instance)
   downloaded_image = URI.open(url)
-  offer.photo.attach(io: downloaded_image, filename: "#{url}.jpg")
+  instance.photo.attach(io: downloaded_image, filename: "#{url}.jpg")
 end
 
 puts "Cleaning database..."
-Offer.destroy_all
 User.destroy_all
+Document.destroy_all
 
 puts "Creating offers and users..."
 User.create!(
@@ -22,8 +22,8 @@ User.create!(
   address: "Shibuya, Tokyo",
   last_name: "Sato",
   phone_number: "08033684952"
-  #email: "carl@email.com",
-  #password: '123456'
+  email: "nobusato@email.com",
+  password: '123456'
 )
 grab_image("https://res.cloudinary.com/djteaofzf/image/upload/v1675845948/Health-pocket/Profile%20pictures/Screen_Shot_2023-02-08_at_17.36.02_kc2zns.png", User.last)
 
@@ -32,8 +32,8 @@ User.create!(
   address: "Arakawa, Tokyo",
   last_name: "De clerck",
   phone_number: "08055684952"
-  #email: "marie@email.com",
-  #password: '123456'
+  email: "marie@email.com",
+  password: '123456'
 )
 grab_image("https://res.cloudinary.com/djteaofzf/image/upload/v1675846612/Health-pocket/Screen_Shot_2023-02-08_at_17.56.31_kvkxk8.png", User.last)
 
@@ -42,8 +42,8 @@ User.create!(
   address: "Yokosuka, Kanagawa",
   last_name: "Johnson",
   phone_number: "08055684952"
-  #email: "marie@email.com",
-  #password: '123456'
+  email: "ranielle@email.com",
+  password: '123456'
 )
 grab_image("https://res.cloudinary.com/djteaofzf/image/upload/v1675845938/Health-pocket/Profile%20pictures/pexels-rodnae-productions-7467950_ulspsr.jpg", User.last)
 
@@ -51,31 +51,32 @@ Document.create!(
   user: User.all.sample,
   type: "Immunization Record",
   country: "Japan",
-  date: "2021/07/12",
-  doctor_name: "Kawaguchi, Ryota",
+  date: Date.parse("2021/07/12"),
+  doctor_name: "Kawaguchi Ryota",
   comment: "2nd COVID buster"
   # picture_url: "v1674729870/Baby Loop/car_e7ztnb.jpg"
 )
-grab_image("", Offer.last)
+grab_image("https://res.cloudinary.com/djteaofzf/image/upload/v1675943448/Health-pocket/Screen_Shot_2023-02-09_at_20.45.45_t1aqhk.png", Document.last)
+#create
 
 Document.create!(
   user: User.all.sample,
   type: "Pathology Records",
   country: "France",
-  date: "2023/01/10",
-  doctor_name: "Marcel, Vincent",
+  date: Date.parse("2023/01/10"),
+  doctor_name: "Marcel Vincent",
   comment: "Blood test checking in French"
   # picture_url: "v1674729870/Baby Loop/car_e7ztnb.jpg"
 )
-grab_image("", Offer.last)
+grab_image("https://res.cloudinary.com/djteaofzf/image/upload/v1675943804/Health-pocket/Screen_Shot_2023-02-09_at_20.55.54_hulxek.png", Document.last)
 
 Document.create!(
   user: User.all.sample,
-  type: "Referral latter",
+  type: "Referral letter",
   country: "United States of America",
-  date: "2022/10/25",
-  doctor_name: "Patel, Mittul",
-  comment: "Referral later for asma"
+  date: Date.parse("2022/10/25"),
+  doctor_name: "Monique Jackson",
+  comment: "Referral letter for asthma"
   # picture_url: "v1674729870/Baby Loop/car_e7ztnb.jpg"
 )
-grab_image("", Offer.last)
+grab_image("https://res.cloudinary.com/djteaofzf/image/upload/v1675943445/Health-pocket/Referral_letter_hwohzn.png", Document.last)
