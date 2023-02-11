a9f403320c5f9dad1dc64cbeb3c4d97411e73c0c
Sarah Vanholsbeeck <vanholsbeeck.sn@gmail.com>
2023-02-11 16:43:00 +0900

    Update .env and ocr_scan.rb

--- app/services/ocr_scan.rb
+++ app/services/ocr_scan.rb
@@ -1,10 +1,10 @@
 require "google/cloud/vision"
+require 'open-uri'
+
 class OcrScan
   def initialize(pdf_url)
     @pdf_url = pdf_url
-    @vision = Google::Cloud::Vision.image_annotator do |config|
-      config.credentials = "http://localhost:3000/lucid-honor-377410-9d5bc28e0192.json"
-    end
+    @vision = Google::Cloud::Vision.image_annotator
   end
 
   def scan
