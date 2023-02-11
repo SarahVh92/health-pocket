2715b8324ddbca47bffe43c70b978bc7548a2061
Sarah Vanholsbeeck <vanholsbeeck.sn@gmail.com>
2023-02-11 12:55:16 +0900

    Partial set-up of Cloud Vision API

--- Gemfile
+++ Gemfile
@@ -59,6 +59,9 @@ gem "simple_form", github: "heartcombo/simple_form"
 
 # Manually added gems
 gem "pundit"
+gem 'google-api-client'
+gem 'google-cloud-vision'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -2,4 +2,29 @@ class DocumentsController < ApplicationController
   def index
     @documents = policy_scope(Document)
   end
+
+  def new
+    @document = Document.new
+    authorize @document
+  end
+
+  def create
+    @document = Document.new(document_params)
+    authorize @document
+
+    @document_info = OcrScan.new(document_params[:photo].tempfile.path).scan
+
+    if @document.save
+      # TO DO: redirect to edit page
+      # redirect_to
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def document_params
+    params.require(:document).permit(:doc_type, :country, :doctor_name, :comment, :date, :photo)
+  end
 end
