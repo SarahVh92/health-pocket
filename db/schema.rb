965b9a6e67861957e033dfea40ce61a983338513
Paulette Zaldivar Flores <paulettz@icloud.com>
2023-10-05 22:44:41 +0900

    attempting to remove splash page

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2023_03_22_125823) do
-
+ActiveRecord::Schema[7.0].define(version: 2023_03_23_115006) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
